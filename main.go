88db72388051d749136049d0d283dd0a8edb2c70
Urr4 <schubert.inf@gmail.com>
2022-09-05 20:05:47 +0200

    Adds movement

--- main.go
+++ main.go
@@ -3,82 +3,157 @@ package main
 import "github.com/gen2brain/raylib-go/raylib"
 
 const (
-    screenWidth = 1000
-    screenHeight = 480
+	screenWidth  = 1000
+	screenHeight = 480
 )
 
 var (
-    running = true
-    backgroundColor = rl.NewColor(147, 211, 196, 255)
+	running = true
 
-    grassSprite rl.Texture2D
-    playerSprite rl.Texture2D
+	backgroundColor = rl.NewColor(147, 211, 196, 255)
+	grassSprite     rl.Texture2D
 
-    playerSrc rl.Rectangle
-    playerDest rl.Rectangle
+	playerSprite                                  rl.Texture2D
+	playerSrc                                     rl.Rectangle
+	playerDest                                    rl.Rectangle
+	playerSpeed                                   float32 = 3
+	playerMoving                                  bool
+	playerDir                                     int
+	playerUp, playerDown, playerLeft, playerRight bool
+	playerFrame                                   int
 
-    playerSpeed float32 = 3
+	frameCount int
+
+	musicPaused bool
+	music       rl.Music
+
+	camera rl.Camera2D
 )
 
-func drawScene(){
-    rl.DrawTexture(grassSprite, 100, 50, rl.White)
-    rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
 }
 
-func input(){
-    if rl.IsKeyDown(rl.KeyW) {
-        playerDest.Y -= playerSpeed
-    }
-    if rl.IsKeyDown(rl.KeyS) {
-        playerDest.Y += playerSpeed
-    }
-    if rl.IsKeyDown(rl.KeyA) {
-        playerDest.X -= playerSpeed
-    }
-    if rl.IsKeyDown(rl.KeyD) {
-        playerDest.X += playerSpeed
-    }
+func input() {
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerMoving = true
+		playerUp = true
+		playerDir = 1
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerMoving = true
+		playerDown = true
+		playerDir = 0
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerMoving = true
+		playerLeft = true
+		playerDir = 2
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerMoving = true
+		playerRight = true
+		playerDir = 3
+	}
+	if rl.IsKeyPressed(rl.KeyQ) {
+		musicPaused = !musicPaused
+	}
 
 }
 
-func update(){
-    running = !rl.WindowShouldClose()
+func update() {
+	running = !rl.WindowShouldClose()
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+
+	if playerMoving {
+		if playerUp {
+			playerDest.Y -= playerSpeed
+		}
+		if playerDown {
+			playerDest.Y += playerSpeed
+		}
+		if playerLeft {
+			playerDest.X -= playerSpeed
+		}
+		if playerRight {
+			playerDest.X += playerSpeed
+		}
+		if frameCount%8 == 0 {
+			playerFrame++
+		}
+	}else if frameCount % 45 == 1 {
+		playerFrame++
+	}
+	frameCount++
+	if playerFrame > 3 {
+		playerFrame = 0
+	}
+	if !playerMoving && playerFrame > 1 {
+		playerFrame = 0
+	}
+
+	playerSrc.Y = playerSrc.Height * float32(playerDir)
+
+
+	rl.UpdateMusicStream(music)
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	}
+
+	camera.Target = rl.NewVector2(playerDest.X-(playerDest.Width/2), playerDest.Y-(playerDest.Height/2))
+
+	playerMoving = false
+	playerUp, playerDown, playerLeft, playerRight = false, false, false, false
 }
 
-func render(){
-    rl.BeginDrawing()
+func render() {
+	rl.BeginDrawing()
 	rl.ClearBackground(backgroundColor)
+	rl.BeginMode2D(camera)
 
 	drawScene()
+
+	rl.EndMode2D()
 	rl.EndDrawing()
 }
 
-//Go calls init funcs before the main func
-func init(){
-    rl.InitWindow(screenWidth, screenHeight, "Funny Game Stuff")
-    rl.SetExitKey(0)
-    rl.SetTargetFPS(60)
+// Go calls init funcs before the main func
+func init() {
+	rl.InitWindow(screenWidth, screenHeight, "Funny Game Stuff")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
 
-    grassSprite = rl.LoadTexture("assets/Tilesets/Grass.png")
-    playerSprite = rl.LoadTexture("assets/Characters/BasicCharakterSpritesheet.png")
+	grassSprite = rl.LoadTexture("assets/Tilesets/Grass.png")
+	playerSprite = rl.LoadTexture("assets/Characters/BasicCharakterSpritesheet.png")
 
-    playerSrc = rl.NewRectangle(0,0,48,48)
-    playerDest = rl.NewRectangle(200,200,100,100)
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	camera = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)), rl.NewVector2(playerDest.X, playerDest.Y), 0.0, 1.0)
+
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("assets/Sound/Farm.mp3")
+	musicPaused = false
+	rl.PlayMusicStream(music)
 }
 
-func quit(){
-    rl.UnloadTexture(grassSprite)
-    rl.UnloadTexture(playerSprite)
-    rl.CloseWindow()
+func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
+	rl.CloseWindow()
 }
 
 func main() {
-
 	for running {
-        input()
-        update()
-        render()
+		input()
+		update()
+		render()
 	}
-
-    quit()
+	quit()
 }
